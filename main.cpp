648bf9cb19d6134ad2b68dffb98eb2ccf83151a5
punkestu <pangestubima89@gmail.com>
2021-02-04 15:11:19 +0700

    membuat class ufo

--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
 #include <SDL.h>
 #include <iostream>
+#include <vector>
+
+class ufo{
+private:
+    SDL_Rect body[2];
+    short moveX, moveY;
+public:
+    ufo(){
+        body[0] = {220,240,40,10};
+        body[1] = {body[0].x+10, body[0].y-10, 20,10};
+        moveX = moveY = 0;
+    }
+    void render(SDL_Renderer* renderer){
+        SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+        SDL_RenderFillRects(renderer, body, 2);
+    }
+};
 
 int main(int argc, char* argv[])
 {
     SDL_Window* window = SDL_CreateWindow("apps", 100,100,480,480,false);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
+    ufo mufo = ufo();
+
     SDL_Event event;
     while(true){
         SDL_SetRenderDrawColor(renderer, 0,0,0,255);
         SDL_RenderClear(renderer);
 
+        mufo.render(renderer);
+
         SDL_PollEvent(&event);
-        if(event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.type == SDLK_ESCAPE)){
+        if(event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)){
             break;
         }
 
