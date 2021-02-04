f01fd6ec4b2a57188c0945953c10ddb10f8b697a
punkestu <pangestubima89@gmail.com>
2021-02-04 15:14:23 +0700

    menambahkan kontrol utama untuk ufo

--- main.cpp
+++ main.cpp
@@ -5,13 +5,50 @@
 class ufo{
 private:
     SDL_Rect body[2];
-    short moveX, moveY;
+    SDL_Rect lightW;
+    bool movD[4];
 public:
     ufo(){
         body[0] = {220,240,40,10};
         body[1] = {body[0].x+10, body[0].y-10, 20,10};
         moveX = moveY = 0;
     }
+
+    void control(SDL_Event* event){
+        if(event->type = SDL_KEYDOWN){
+            if(event->key.keysym.sym == SDLK_a){
+                movD[0] = 1;
+            }
+            if(event->key.keysym.sym == SDLK_d){
+                movD[1] = 1;
+            }
+            if(event->key.keysym.sym == SDLK_w){
+                movD[2] = 1;
+            }
+            if(event->key.keysym.sym == SDLK_s){
+                movD[3] = 1;
+            }
+        }
+        if(event->type = SDL_KEYUP){
+            if(event->key.keysym.sym == SDLK_a){
+                movD[0] = 0;
+            }
+            if(event->key.keysym.sym == SDLK_d){
+                movD[1] = 0;
+            }
+            if(event->key.keysym.sym == SDLK_w){
+                movD[2] = 0;
+            }
+            if(event->key.keysym.sym == SDLK_s){
+                movD[3] = 0;
+            }
+        }
+    }
+
+    void update(){
+
+    }
+
     void render(SDL_Renderer* renderer){
         SDL_SetRenderDrawColor(renderer, 255,255,255,255);
         SDL_RenderFillRects(renderer, body, 2);
